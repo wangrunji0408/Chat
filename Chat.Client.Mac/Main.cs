eeace33b73d079dab13fc5455970ddeec0b0ef18
WangRunji <wangrunji0408@163.com>
2017-08-28 15:54:12 +0800

    New: Client.Mac. But it can not run due to grpc throwing NotImplementedException.

---
+++ Chat.Client.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Chat.Client.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
